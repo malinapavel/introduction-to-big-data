671b7904258207b719c8720f57475f37a70f9cb7
malinapavel <malina.pavel@student.upt.ro>
2023-07-11 18:15:23 +0300

    Students statistics based on the receiving countries

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
 
 
-spark = SparkSession.builder\
-                    .master("local[*]")\
-                    .appName('Erasmus Students Data')\
-                    .config("spark.driver.bindAddress", "localhost")\
-                    .config("spark.ui.port", "4040")\
-                    .getOrCreate()
+spark = SparkSession.builder \
+    .master("local[*]") \
+    .appName('Erasmus Students Data') \
+    .config("spark.driver.bindAddress", "localhost") \
+    .config("spark.ui.port", "4040") \
+    .getOrCreate()
 
-def test_pyspark():
-    # CSV -> PySpark DataFrame -> Pandas DataFrame
-    df_spark = spark.read\
-                    .options(header=True, inferSchema=True, delimiter=',')\
-                    .csv('Erasmus.csv')
-
-    df_spark.show(n=100)
+# CSV -> PySpark DataFrame
+df_spark = spark.read \
+                .options(header=True, inferSchema=True, delimiter=',') \
+                .csv('Erasmus.csv')
 
 
 
-test_pyspark()
+def erasmus_data_display():
+    df_student_cnt = df_spark.groupBy(["Receiving Country Code", "Sending Country Code"]) \
+                             .count()
+    df_student_cnt = df_student_cnt.orderBy("Receiving Country Code", "Sending Country Code")
 
+    print('\n\n')
+    print("~ Number of students that went on an Erasmus mobility, based on every Receiving Country Code")
+    df_student_cnt.show(n=df_student_cnt.count())
+
+    print('\n\n')
+    print("~ Number of students that went on an Erasmus mobility, based on a Receiving Country Code from the following: LV, MK, MT")
+    df_student_cnt.where(col("Receiving Country Code").isin(["LV", "MK", "MT"])).show(n=50)
+
+
+
+
+
+erasmus_data_display()
